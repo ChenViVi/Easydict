c0e05c0c2ac56587c4642445ad8f1f6a567ae7ce
ValoraChen <932942491@qq.com>
2024-03-14 16:55:32 +0800

    del TencentService

--- Easydict/Feature/Service/Model/EZServiceTypes.m
+++ Easydict/Feature/Service/Model/EZServiceTypes.m
@@ -57,7 +57,6 @@ static EZServiceTypes *_instance;
                                            EZServiceTypeBaidu, [EZBaiduTranslate class],
                                            EZServiceTypeBing, [EZBingService class],
 //                                           EZServiceTypeCaiyun, [EZCaiyunService class],
-//                                           EZServiceTypeTencent, [EZTencentService class],
 //                                           EZServiceTypeAli, [EZAliService class],
 //                                           EZServiceTypeGemini, [EZGeminiService class],
                                            nil];
