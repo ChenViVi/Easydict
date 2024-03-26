b9d32645e8c58fcf8a32f7822bb6056612f25813
ValoraChen <932942491@qq.com>
2024-03-26 17:16:45 +0800

    Update MenuView.swift

--- Easydict/NewApp/View/MenuView.swift
+++ Easydict/NewApp/View/MenuView.swift
@@ -12,106 +12,132 @@ struct MenuView: View {
     var body: some View {
         VStack {
             VStack {
-                HStack {
-                    ZStack {
-                        Color.accentColor
-                        Image(systemName: "keyboard").foregroundColor(.white)
+                Button(action: {}) {
+                    HStack {
+                        ZStack {
+                            Color.accentColor
+                            Image(systemName: "keyboard").foregroundColor(.white)
+                        }
+                        .cornerRadius(13)
+                        .frame(width: 26, height: 26)
+                        VStack(alignment: .leading) {
+                            Text("Input Translate")
+                                .font(.headline)
+                                .fontWeight(.regular)
+                                .foregroundColor(Color.black)
+                            Text("⎇ A")
+                                .font(.subheadline)
+                                .foregroundColor(Color.secondary)
+                        }
+                        Spacer()
                     }
-                    .cornerRadius(13)
-                    .frame(width: 26, height: 26)
-                    VStack(alignment: .leading) {
-                        Text("Input Translate")
-                            .font(.headline)
-                            .fontWeight(.regular)
-                        Text("⎇ A")
-                            .font(.subheadline)
-                            .foregroundColor(Color.secondary)
-                    }
-                    Spacer()
                 }
-                HStack {
-                    ZStack {
-                        Color.accentColor
-                        Image(systemName: "camera.viewfinder").foregroundColor(.white)
+                .buttonStyle(.borderless)
+                Button(action: {}) {
+                    HStack {
+                        ZStack {
+                            Color.accentColor
+                            Image(systemName: "camera.viewfinder").foregroundColor(.white)
+                        }
+                        .cornerRadius(13)
+                        .frame(width: 26, height: 26)
+                        VStack(alignment: .leading) {
+                            Text("Screenshot Translate")
+                                .font(.headline)
+                                .fontWeight(.regular)
+                                .foregroundColor(Color.black)
+                            Text("⎇ S")
+                                .font(.subheadline)
+                                .foregroundColor(Color.secondary)
+                        }
+                        Spacer()
                     }
-                    .cornerRadius(13)
-                    .frame(width: 26, height: 26)
-                    VStack(alignment: .leading) {
-                        Text("Screenshot Translate")
-                            .font(.headline)
-                            .fontWeight(.regular)
-                        Text("⎇ S")
-                            .font(.subheadline)
-                            .foregroundColor(Color.secondary)
-                    }
-                    Spacer()
                 }
-                HStack {
-                    ZStack {
-                        Color.accentColor
-                        Image(systemName: "highlighter").foregroundColor(.white)
+                .buttonStyle(.borderless)
+                Button(action: {}) {
+                    HStack {
+                        ZStack {
+                            Color.accentColor
+                            Image(systemName: "highlighter").foregroundColor(.white)
+                        }
+                        .cornerRadius(13)
+                        .frame(width: 26, height: 26)
+                        VStack(alignment: .leading) {
+                            Text("Select Translate")
+                                .font(.headline)
+                                .fontWeight(.regular)
+                                .foregroundColor(Color.black)
+                            Text("⎇ D")
+                                .font(.subheadline)
+                                .foregroundColor(Color.secondary)
+                        }
+                        Spacer()
                     }
-                    .cornerRadius(13)
-                    .frame(width: 26, height: 26)
-                    VStack(alignment: .leading) {
-                        Text("Select Translate")
-                            .font(.headline)
-                            .fontWeight(.regular)
-                        Text("⎇ D")
-                            .font(.subheadline)
-                            .foregroundColor(Color.secondary)
-                    }
-                    Spacer()
                 }
+                .buttonStyle(.borderless)
             }
             .padding()
             .background(Color.bgMenuItem)
             .cornerRadius(10)
             .shadow(color: Color.black.opacity(0.3), radius: 10, x: 0, y: 10)
-            HStack {
-                ZStack {
-                    Color.accentColor
-                    Image(systemName: "dot.viewfinder").foregroundColor(.white)
+            Button(action: {}) {
+                HStack {
+                    ZStack {
+                        Color.accentColor
+                        Image(systemName: "dot.viewfinder").foregroundColor(.white)
+                    }
+                    .cornerRadius(13)
+                    .frame(width: 26, height: 26)
+                    VStack(alignment: .leading) {
+                        Text("Silent Screenshot OCR")
+                            .font(.headline)
+                            .fontWeight(.regular)
+                            .foregroundColor(Color.black)
+                        Text("⎇ ⇧ S")
+                            .font(.subheadline)
+                            .foregroundColor(Color.secondary)
+                    }
+                    Spacer()
                 }
-                .cornerRadius(13)
-                .frame(width: 26, height: 26)
-                VStack(alignment: .leading) {
-                    Text("Silent Screenshot OCR")
-                        .font(.headline)
-                        .fontWeight(.regular)
-                    Text("⎇ ⇧ S")
-                        .font(.subheadline)
-                        .foregroundColor(Color.secondary)
-                }
-                Spacer()
+                .padding()
+                .background(Color.bgMenuItem)
+                .cornerRadius(10)
+                .shadow(color: Color.black.opacity(0.3), radius: 10, x: 0, y: 10)
             }
-            .padding()
-            .background(Color.bgMenuItem)
-            .cornerRadius(10)
-            .shadow(color: Color.black.opacity(0.3), radius: 10, x: 0, y: 10)
+            .buttonStyle(.borderless)
             HStack(spacing: 10) {
-                VStack {
-                    Spacer()
-                    Image(systemName: "gear")
-                    Text("Setting")
-                    Spacer()
+                Button(action: {}) {
+                    VStack {
+                        Spacer()
+                        Image(systemName: "gear")
+                            .foregroundColor(Color.black)
+                        Text("Setting")
+                            .foregroundColor(Color.black)
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.bgMenuItem)
+                    .cornerRadius(10)
+                    .shadow(color: Color.black.opacity(0.3), radius: 10, x: 0, y: 10)
                 }
-                .frame(maxWidth: .infinity)
-                .padding()
-                .background(Color.bgMenuItem)
-                .cornerRadius(10)
-                .shadow(color: Color.black.opacity(0.3), radius: 10, x: 0, y: 10)
-                VStack {
-                    Spacer()
-                    Image(systemName: "rectangle.portrait.and.arrow.right")
-                    Text("Quit")
-                    Spacer()
+                .buttonStyle(.borderless)
+                Button(action: {}) {
+                    VStack {
+                        Spacer()
+                        Image(systemName: "rectangle.portrait.and.arrow.right")
+                            .foregroundColor(Color.black)
+                        Text("Quit")
+                            .foregroundColor(Color.black)
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.bgMenuItem)
+                    .cornerRadius(10)
+                    .shadow(color: Color.black.opacity(0.3), radius: 10, x: 0, y: 10)
                 }
-                .frame(maxWidth: .infinity)
-                .padding()
-                .background(Color.bgMenuItem)
-                .cornerRadius(10)
-                .shadow(color: Color.black.opacity(0.3), radius: 10, x: 0, y: 10)
+                .buttonStyle(.borderless)
             }
         }
         .padding()
